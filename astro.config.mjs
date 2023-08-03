9db9bdaa86e2067273782df6c7d676fa87f00a22
Nicolas Toffolo <nicolas.toffolo@mirahi.io>
2023-08-03 17:24:52 +0200

    deps:Added react, shadcn deps; content: Added won test content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  })]
+  }), react()]
 });
---
+++ src/pages/post.md
@@ -0,0 +1,7 @@
+---
+layout: ../../layouts/BlogPostLayout.astro
+title: Astro in brief
+author: Himanshu
+description: Find out what makes Astro awesome!
+---
+This is a post written in Markdown.
