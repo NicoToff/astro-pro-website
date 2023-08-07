5b315abf520508cb4df599c9008d2385873866c6
Nicolas Toffolo <nicolas.toffolo@mirahi.io>
2023-08-07 12:07:19 +0200

    fix: Nav now justifies center; minor: design changes

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -9,8 +9,13 @@ import {
 export function Nav({ children }: { children?: React.ReactNode }) {
   const style = navigationMenuTriggerStyle();
   return (
-    <NavigationMenu>
+    <NavigationMenu className="max-w-none mx-4 mt-4 mb-2 pb-2 border-b">
       <NavigationMenuList>
+        <NavigationMenuItem className="px-2">
+          <NavigationMenuLink href="/">
+            <img src="/logos/nt256.png" alt="logo" className="h-9 w-9" />
+          </NavigationMenuLink>
+        </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink href="/" className={style}>
             Home
