246f4c2d9885b53ce6e7e568427b0c15e88a35e1
Nicolas Toffolo <nicolas.toffolo@mirahi.io>
2023-08-07 17:18:49 +0200

    Added a proper footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,30 @@
+import { Github, Linkedin, type LucideIcon } from "lucide-react";
+
+export function Footer() {
+  const listItems = [
+    { href: "https://github.com/NicoToff", Icon: Github },
+    { href: "https://www.linkedin.com/in/toffolo01/", Icon: Linkedin },
+  ] satisfies { href: string; Icon: LucideIcon }[];
+
+  return (
+    <footer className="mt-8 m-4 border-t">
+      <div className="w-full mx-auto max-w-screen-lg items-center p-4 flex justify-between">
+        <div>
+          <a href="/" className="hover:underline">
+            © <span data-current-year>2023</span>
+            {" — NicoToff"}
+          </a>
+        </div>
+        <ul className="flex gap-2">
+          {listItems.map(({ href, Icon }) => (
+            <li className="p-2 hover:bg-secondary rounded-md" key={href}>
+              <a href={href} target="_blank" rel="noopener noreferrer">
+                <Icon size={48} />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
+}
--- src/layouts/page.ts
+++ src/layouts/page.ts
@@ -3,3 +3,8 @@ import { setColorThemeOnPageLoad } from "@/lib/color-theme";
 if (typeof window !== "undefined") {
   setColorThemeOnPageLoad();
 }
+
+const yearSpan = document.querySelector("[data-current-year]");
+if (yearSpan) {
+  yearSpan.innerHTML = new Date().getFullYear().toString();
+}
