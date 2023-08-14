60b3465a9727a05792be7287a3822f4a42ed6f3b
Nicolas Toffolo <nicolas.toffolo@mirahi.io>
2023-08-14 08:07:31 +0200

    Removed broken GH actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nicotoff.github.io",
-  base: "/",
   integrations: [
     tailwind({
       applyBaseStyles: false,
