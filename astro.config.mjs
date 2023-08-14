9bc50afd911718ca1546b1dd501e693cba839c48
Nicolas Toffolo <nicolas.toffolo@mirahi.io>
2023-08-14 07:53:29 +0200

    Updated GH pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nicotoff.github.io",
+  base: "/",
   integrations: [
     tailwind({
       applyBaseStyles: false,
