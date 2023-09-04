e3edb9367526bc23e767aacfed0f594b083565a2
Nicolas Toffolo <nicolas.toffolo@mirahi.io>
2023-09-04 15:12:36 +0200

    content: added skeleton and first debounce

--- src/components/spells-api/search.tsx
+++ src/components/spells-api/search.tsx
@@ -1,46 +1,64 @@
 import { Input } from "@/shadcn/ui/input";
 import { useEffect, useState, type ChangeEvent } from "react";
 
+import { Skeleton } from "@/shadcn/ui/skeleton";
 import type { components } from "@/types/spells-api";
-import { SpellCard } from "./spell-card";
+import { SpellCard, SpellCardSkeleton } from "./spell-card";
 
 type Spell = components["schemas"]["Spell"];
 
 export function Search() {
   //   const url = "https://nestjs-spells-api.fly.dev/spells" as const;
-  const url = "http://localhost:8000/spells" as const;
+  const url = "https://nestjs-spells-api.fly.dev/spells" as const;
 
   const [value, setValue] = useState("");
   const [spells, setSpells] = useState<Spell[]>([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   function onChange(e: ChangeEvent<HTMLInputElement>) {
     setValue(e.target.value);
+    setSpells([]);
+    setIsFetching(true);
   }
 
   useEffect(() => {
-    if (!value.length) {
+    if (!value) {
       setSpells([]);
+      setIsFetching(false);
       return;
     }
-    const params = new URLSearchParams({ name: value });
-    const fullUrl = `${url}?${params.toString()}`;
-    fetch(fullUrl)
-      .then((res) => res.json() as Promise<Spell[]>)
-      .then((spells) => setSpells(spells))
-      .catch((err) => {
-        console.error(err);
-        setSpells([]);
-      });
+
+    const timeoutId = setTimeout(() => {
+      console.log("fetching");
+      const params = new URLSearchParams({ name: value });
+      fetch(`${url}?${params.toString()}`)
+        .then((res) => res.json() as Promise<Spell[]>)
+        .then((spells) => setSpells(spells))
+        .catch((err) => {
+          console.error(err);
+          setSpells([]);
+        })
+        .finally(() => setIsFetching(false));
+    }, 500);
+
+    console.log("setting timeout", timeoutId);
+
+    return () => clearTimeout(timeoutId);
   }, [value]);
 
   return (
     <>
-      <Input value={value} onChange={onChange} placeholder="Search..." />
-      {spells.length ? (
-        <div className="mt-2 grid grid-cols-3 gap-2">
-          {spells.map((s) => (
-            <SpellCard key={s.name} spell={s} />
-          ))}
+      <Input value={value} onChange={onChange} placeholder="Search by name..." />
+      {isFetching || spells.length ? (
+        <div className="mt-2 grid gap-2 lg:grid-cols-2">
+          {spells.length !== 0 ? (
+            spells.map((s) => <SpellCard key={s.name} spell={s} />)
+          ) : (
+            <>
+              <SpellCardSkeleton />
+              <SpellCardSkeleton />
+            </>
+          )}
         </div>
       ) : null}
     </>
--- src/components/spells-api/spell-card.tsx
+++ src/components/spells-api/spell-card.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/shadcn/ui/card";
+import { Skeleton } from "@/shadcn/ui/skeleton";
 import type { components } from "@/types/spells-api";
 
 type Spell = components["schemas"]["Spell"];
@@ -16,15 +17,27 @@ export function SpellCard({ spell, className }: { spell: Spell; className?: stri
           <ul>
             <HeaderListItem label="Casting Time" value={spell.castingTime} />
             <HeaderListItem label="Range" value={spell.range} />
+            {spell.area ? <HeaderListItem label="Area" value={spell.area} /> : null}
             <HeaderListItem label="Components" value={formatComponents(spell)} />
-            <HeaderListItem label="Duration" value={spell.duration} />
+            <HeaderListItem label="Duration" value={formatDuration(spell)} />
           </ul>
+          {spell.flavor ? <p className="mt-4 font-serif italic">{spell.flavor}</p> : null}
         </CardDescription>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex flex-col gap-2">
         {spell.description.map((desc, i) => (
           <p key={i}>{desc}</p>
         ))}
+        {spell.atHigherLevels ? (
+          <p>
+            <span className="font-bold">At Higher Levels.</span> {spell.atHigherLevels}
+          </p>
+        ) : null}
+        {spell.cantripUpgrade ? (
+          <p>
+            <span className="font-bold">Cantrip Upgrade.</span> {spell.cantripUpgrade}
+          </p>
+        ) : null}
       </CardContent>
       <CardFooter className="text-sm text-muted-foreground">{`Sources: ${spell.sources.join(", ")}`}</CardFooter>
     </Card>
@@ -57,4 +70,37 @@ function HeaderListItem({ label, value }: { label: string; value: string }) {
 
 function formatComponents(spell: Spell) {
   return `${spell.components.join(", ")}${spell.material ? ` (${spell.material})` : ""}`;
+}
+
+function formatDuration(spell: Spell) {
+  const duration = spell.duration;
+  return spell.concentration ? `Concentration, up to ${duration.toLowerCase()}` : duration;
+}
+
+export function SpellCardSkeleton() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="mb-2 flex flex-col gap-1">
+          <Skeleton className="h-8 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+        </CardTitle>
+        <CardDescription className="flex flex-col gap-1">
+          <Skeleton className="h-5 w-3/5" />
+          <Skeleton className="h-5 w-3/5" />
+          <Skeleton className="h-5 w-4/5" />
+          <Skeleton className="h-5 w-3/5" />
+          <Skeleton className="mt-3 h-4" />
+          <Skeleton className="h-4 w-1/2" />
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-2">
+        <Skeleton className="h-5" />
+        <Skeleton className="h-5" />
+        <Skeleton className="h-5" />
+        <Skeleton className="h-5" />
+        <Skeleton className="h-5 w-1/2" />
+      </CardContent>
+    </Card>
+  );
 }
