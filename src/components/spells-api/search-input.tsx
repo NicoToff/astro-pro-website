a2f6c1e27b7fa0123a67e52681baa91a4e72f290
Nicolas Toffolo <nicolas.toffolo@mirahi.io>
2023-09-05 10:32:51 +0200

    chore(cleanup): search input

--- src/components/spells-api/search-input.tsx
+++ src/components/spells-api/search-input.tsx
@@ -1,25 +1,28 @@
 import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
 import { Input, type InputProps } from "@/shadcn/ui/input";
-import { Search, Loader, SearchX, Disc3 } from "lucide-react";
+import { Search, Disc3 } from "lucide-react";
 
 export interface SearchInputProps extends InputProps {
   icon?: React.ReactNode;
   iconPlacement?: "left" | "right";
   isLoading?: boolean;
+  inputClassName?: string;
+  iconDivClassName?: string;
 }
 
 const SearchInput = forwardRef<HTMLInputElement, SearchInputProps>(
-  ({ className, icon, iconPlacement = "left", isLoading, ...props }, ref) => {
+  ({ className, icon, iconPlacement = "left", isLoading, inputClassName, iconDivClassName, ...props }, ref) => {
     if (!icon) icon = <Search size={24} />;
     if (isLoading) icon = <Disc3 className="animate-spin" size={24} />;
     return (
       <div className={cn("relative", className)}>
-        <Input ref={ref} className={cn("pl-10", iconPlacement === "right" ? "pr-10" : null)} {...props} />
+        <Input ref={ref} className={cn(iconPlacement === "right" ? "pr-10" : "pl-10", inputClassName)} {...props} />
         <div
           className={cn(
             "pointer-events-none absolute inset-y-0 flex items-center",
-            iconPlacement === "right" ? "right-2" : "left-2"
+            iconPlacement === "right" ? "right-2" : "left-2",
+            iconDivClassName
           )}
         >
           {icon}
