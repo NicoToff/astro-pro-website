9d3391514f589aebbdde121bc418554e47d33081
Nicolas Toffolo <nicolas.toffolo@mirahi.io>
2023-09-07 08:47:53 +0200

    test(os): browserstack

--- src/components/spells-api/search.tsx
+++ src/components/spells-api/search.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useFilters } from "./use-filters.ts";
 
 import { SearchResults } from "./search-results.tsx";
@@ -9,11 +9,17 @@ import type { Spell } from "@/types/spell.ts";
 export type SearchProps = { url: string };
 export function Search({ url }: SearchProps) {
   const [spells, setSpells] = useState<Spell[]>([]);
+  const [userAgent, setUserAgent] = useState<string>("");
+
+  useEffect(() => {
+    if (window) setUserAgent(window.navigator.userAgent);
+  }, []);
 
   const filters = useFilters({ url, setResult: setSpells });
 
   return (
     <>
+      <div>Viewing this on: {userAgent}</div>
       <SearchFilters {...filters} />
       <SearchResults spells={spells} {...filters} />
     </>
